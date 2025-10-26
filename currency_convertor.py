bbea56fd7c66b970eac88645ac7c04dbfa9bd005
code_ninja <instructor@codeninja.sg>
2025-10-26 13:12:39 +0800

    Create currency_convertor.py

---
+++ currency_convertor.py
@@ -0,0 +1,18 @@
+print("currency Converter")
+print("1.sgd to usd")
+print("2.sgd to myr")
+print("3. sgd to MYR")
+print("4. sgd to bnd")
+print("5. quit")
+choice=input("pick a number (1/2/3/4/5:")
+amount_sgd=float(input("how much in sgd"))
+
+rate_usd=0.74
+rate_myr=3.15
+rate_idr=10500
+rate_bind=1.00
+
+
+if choice==1
+   total=amount_sgd*rate_usd
+print("{}sgd is")
