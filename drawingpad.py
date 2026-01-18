582acac2b5a51712e6a378f5c459040a0b8d380c
code_ninja <instructor@codeninja.sg>
2026-01-18 13:05:11 +0800

    Add files via upload

---
+++ drawingpad.py
@@ -0,0 +1,36 @@
+import=tinker as tk
+
+root=tk.TK()
+root.title("drawing Pad")
+root.geometry("800x600")
+
+
+controlss=tk.TK()
+root.title("Drawing Pad")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
