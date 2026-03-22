3fdb0f081fef6203b33c5a5786554f80d0c325df
code_ninja <instructor@codeninja.sg>
2026-03-22 13:11:57 +0800

    Add files via upload

---
+++ Snake.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import random
+
+root=tk.Tk()
+root.title('Snake-1')
+
+SIZE = 20
+W = 400
+H = 400
+
+canvas = tk.Canvas(root,width=W,height=H,bg='white')
+canvas.pack()
+
+snake=[(10,10)]
+
+dx = 1
+dy = 0
+ 
+food = (random.randint(0, W//SIZE - 1),
+       random.randint(0, H//SIZE - 1))
+
+def draw():
+    canvas.delete ('all')
+
+    fx,fy = food
+    canvas.create_rectangle(fx*SIZE, fy*SIZE, fx*SIZE+SIZE,fy*SIZE+SIZE,fill='red') 
+    
+    for (x,y) in snake:
+        canvas.create_rectangle(x*SIZE,y*SIZE,x*SIZE+SIZE,y*SIZE+SIZE,fill='green')
+
+def game_loop():
+    global snake,food
+
+    head_x,head_y=snake[0]
+    new_head=(head_x+dx,head_y+dy)
+    snake.insert(0,new_head)
+
+if new_head==food:
+    food = (random.randint(0,W//SIZE - 1),
+            random.randint(0,H//SIZE - 1))
+else:
+    snake.pop()
+
+draw()
+root.after(150,game_loop)        
+
+draw()
+root.mainloop()
