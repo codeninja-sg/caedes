9b448f4ec4bd668a76d604292d5aab949bf0713f
code_ninja <instructor@codeninja.sg>
2026-04-12 13:02:09 +0800

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -4,9 +4,9 @@ import random
 root=tk.Tk()
 root.title('Snake-1')
 
-SIZE = 40
-W = 800
-H = 800
+SIZE = 20
+W = 400
+H = 400
 
 canvas = tk.Canvas(root,width=W,height=H,bg='white')
 canvas.pack()
@@ -30,9 +30,23 @@ def draw():
 
 def game_loop():
     global snake,food
+    max_x=W//SIZE
+    max_y=H//SIZE
 
     head_x,head_y=snake[0]
-    new_head=(head_x+dx,head_y+dy)
+    new_head = (head_x+dx,head_y+dy)
+    
+
+
+    if new_head[0]< 0 or new_head[0] >= max_x or new_head[1] < 0 or new_head[1] >= max_y or new_head in snake:
+        print("Game Over")
+        root.quit()
+    
+
+    if new_head in snake:
+        print("Game Over")
+        root.quit()
+
     snake.insert(0,new_head)
 
     if new_head==food:
@@ -60,13 +74,28 @@ def right(event):
     global dx, dy
     dx, dy = 1, 0
 
+def restart():
+     global snake, dx, dy, food, game_over
+     # snake = [(10, 10)]
+     dx, dy = 1, 0
+
+     food = (random.randint(0, max_x - 1),
+         random.randint(0, max_y - 1))
+    
+     game_over = False
+     #status_label.config(text="")
+     draw()
+     root.after(150, game_loop)
+
 root.bind("<Up>",up)
 root.bind("<Down>",down)
 root.bind("<Left>",left)
 root.bind("<Right>",right)
 
-draw()
-root.after(150, game_loop)
+restart_btn = tk.Button(root, text="Restart", command=restart)                        
+restart_btn.pack(pady=5)
 
+game_loop()
 draw()
 root.mainloop()
+
