ecf2ef6de4d6c327a2e09d445de5a6329a248ce8
code_ninja <instructor@codeninja.sg>
2026-06-21 13:05:22 +0800

    Add files via upload

---
+++ Lights out.py
@@ -0,0 +1,25 @@
+grid = [
+[1,1,1,1,1],
+[1,1,1,1,1],
+[1,1,1,1,1],
+[1,1,1,1,1]
+]
+
+ROWS = 5
+COLS = 5
+
+grid=[[0]* COLS for_ in range(ROWS)]
+
+import tkinter as tk
+import random
+
+root = tk.TK()
+root.title('Lights Out')
+
+ROWS = 5
+COLS = 5
+
+grid=[[0]* COLS for_ in range(ROWS)]
+
+buttons = []
+
